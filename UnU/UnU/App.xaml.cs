86a770ed0a7670074360170094a42406aecb600d
Dj-klk12 <frank.calcina@ucsp.edu.pe>
2024-06-25 19:24:55 -0500

    Agregar archivos de proyecto.

---
+++ UnU/UnU/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using generarcv.View;
+using Xamarin.Forms.Xaml;
+
+namespace generarcv
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new UnicaPag(
+                "Nombre", "Apellido", "Dirección", "correo@example.com", "1234567890",
+                "Experiencia 1", "Experiencia 2", "Experiencia 3", "Nivel de inglés",
+                "Educación", "Aptitud 1", "Aptitud 2", "Aptitud 3", "Descripción"));
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ UnU/UnU/View/SegundaPag.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace generarcv.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class UnicaPag : ContentPage
+    {
+        public UnicaPag(string nombre, string apellido, string direcc, string correo, string contacto, string expe1, string expe2, string expe3, string ingles, string educacion, string aptitud1, string aptitud2, string aptitud3, string descripcion)
+        {
+            InitializeComponent();
+            LabelNombreComp.Text = "Frank Eduardo Calcina Arias";
+            direc.Text = "Actualmente vivo en --> Arequipa-Paucarpata " ;
+            corr.Text = "Puedes enviarme un correo --> frank.calcina@ucsp.edu.pe" ;
+            numero.Text = "Mi WhatsApp es --> 999 999 898" + " suelo responder rápido";
+            Experiencia1.Text = "♦ Google ";
+            Experiencia2.Text = "♦ BCP " ;
+            Experiencia3.Text = "♦ Interbank " ;
+            Ingles.Text = "Mi nivel de inglés es --> Intermedio " ;
+            edu.Text = "SUPERIOR UCSP CS";
+            Ap1.Text = "♦ Liderazgo" ;
+            Ap2.Text = "♦ Proactividad" ;
+            Ap3.Text = "♦ Organización" ;
+            Desc.Text = "Bueno en lo que hago y trabajo en grupo";
+        }
+    }
+}
